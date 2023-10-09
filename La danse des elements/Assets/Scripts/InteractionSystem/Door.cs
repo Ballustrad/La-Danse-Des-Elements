0f36c6aebc74904edfd3d0a7b336507e220a2fd1
Galaan <galaan.rouzier@e-artsup.net>
2023-10-09 12:19:50 +0200

    Update + Nouveau Script
    Update :
    - Door
    - Chest
    Nouveau Script :
    Inventory

--- La danse des elements/Assets/Scripts/InteractionSystem/Door.cs
+++ La danse des elements/Assets/Scripts/InteractionSystem/Door.cs
@@ -2,7 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour
+public class Door : MonoBehaviour, IInteractable
 {
+    [SerializeField] private string _prompt;
+
+    string IInteractable.InteractionPrompt => _prompt;
+
+    bool IInteractable.Interact(Interactor interactor)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log(message: "Opening Door!");
+        return true;
+    }
     
 }
