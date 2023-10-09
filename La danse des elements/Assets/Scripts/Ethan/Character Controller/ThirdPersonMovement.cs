65b4b9766e4ffa5999d06b54480aa0eef299eb01
Ballustrad <ethan.azoulay@e-artsup.net>
2023-10-09 12:11:05 +0200

    le controller

--- La danse des elements/Assets/Scripts/Ethan/Character Controller/ThirdPersonMovement.cs
+++ La danse des elements/Assets/Scripts/Ethan/Character Controller/ThirdPersonMovement.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed;
+    public Transform cam;
+
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity; 
+
 
 
     // Update is called once per frame
@@ -17,7 +24,13 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * speed * Time.deltaTime);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
